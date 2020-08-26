e325573cfca2a826cffa64a2f7df4477e6aa0833
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2020-08-26 09:32:55 +0200

    first commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Ivan Ruiz Monjo on 26/08/2020.
+//  Copyright © 2020 Ivan Ruiz Monjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
